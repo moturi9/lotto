f541f7644db01f72c204cb0b4629449d67595b0b
moturi9 <moturi9@yahoo.com>
2020-04-02 22:26:37 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# lotto
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return <div>nice</div>
+  }
+}
+
+/* export the component to be used in other components */
+export default App
