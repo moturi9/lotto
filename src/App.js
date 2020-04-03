95480fd0672d2d0e89c39fda67dee6a8133cbbb5
moturi9 <moturi9@yahoo.com>
2020-04-02 23:22:28 -0500

    lottery component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-
+// import Ball from './Ball'
+import Lottery from './Lottery'
 import './App.css'
 
 class App extends Component {
   render() {
-    return <div>nice</div>
+    return (
+      <div className="App">
+        <Lottery />
+        <Lottery title="Mini Daily" numBalls={4} maxNum={10} />
+      </div>
+    )
   }
 }
 
